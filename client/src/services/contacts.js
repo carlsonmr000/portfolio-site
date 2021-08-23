622819e84a03715473bdd4dfa2d1b9c8b26f4fd4
carlsonmr <carlsonmr000@gmail.com>
2021-08-23 15:36:04 -0500

    added components and service files

---
+++ client/src/services/contacts.js
@@ -0,0 +1,24 @@
+import api from "./apiConfig";
+
+export const getAllMessages = async () => {
+  const res = await api.get("/contacts");
+  return res.data;
+};
+
+export const getOneMessage = async (id) => {
+  const res = await api.get(`/contacts/${id}`);
+  return res.data;
+};
+
+export const deleteMessage = async (id) => {
+  await api.delete(`/contacts/${id}`);
+};
+
+export const createMessage = async (contactData) => {
+  try {
+    const res = await api.post("/contacts", { contact: contactData });
+    return res.data;
+  } catch {
+    return false
+  }
+};
