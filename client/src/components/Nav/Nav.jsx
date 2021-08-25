903133c301c02b9ce5b7527c94802d38fbab0d25
carlsonmr <carlsonmr000@gmail.com>
2021-08-25 00:33:56 -0500

    added resume to navbar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,31 +1,84 @@
 import "./Nav.css";
-import {Link} from 'react-scroll';
-
- 
+import { Link } from "react-scroll";
 
 const Nav = () => {
-
-  
-
-
   return (
     <header className="navbar">
       <nav>
- 
         <div>
-        <a><Link  href="/" className="item" to="home" spy={true} smooth={true} duration={100}>Home</Link></a>
+          <a>
+            <Link
+              href="/"
+              className="item"
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={100}
+            >
+              Home
+            </Link>
+          </a>
 
-    
-          <a><Link  href="/#about" className="item" to="about" spy={true} smooth={true} duration={100}>About</Link></a>
- 
-          <a><Link  href="/#skills" className="item" to="skills" spy={true} smooth={true} duration={100}>Skills</Link></a>
+          <a>
+            <Link
+              href="/#about"
+              className="item"
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={100}
+            >
+              About
+            </Link>
+          </a>
 
+          <a>
+            <Link
+              href="/#skills"
+              className="item"
+              to="skills"
+              spy={true}
+              smooth={true}
+              duration={100}
+            >
+              Skills
+            </Link>
+          </a>
 
-          <a><Link  href="/#projects" className="item" to="projects" spy={true} smooth={true} duration={100}>Projects</Link></a>
+          <a>
+            <Link
+              href="/#projects"
+              className="item"
+              to="projects"
+              spy={true}
+              smooth={true}
+              duration={100}
+            >
+              Projects
+            </Link>
+          </a>
 
+          <a
+            href="https://docs.google.com/document/d/12_ODj_FEViDGPyeR9Q7z_eZ1cT4dI1ctZVr3yNTwk7w/preview"
+            target="_blank"
+            rel="noreferrer"
+            className="item"
+          >
+            Resume
+          </a>
 
-          <a><Link  href="/#contact" className="item" to="contact" spy={true} smooth={true} duration={100}>Contact</Link></a>
-
+          <a>
+            <Link
+              href="/#contact"
+              className="item"
+              to="contact"
+              spy={true}
+              smooth={true}
+              duration={100}
+            >
+              Contact
+            </Link>
+          </a>
         </div>
       </nav>
     </header>
