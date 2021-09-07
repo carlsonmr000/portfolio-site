631475744c8a63d864fb2c4e887e2dba6e5a05a9
carlsonmr <carlsonmr000@gmail.com>
2021-09-07 17:27:50 -0500

    added resume component

---
+++ client/src/components/Resume/Resume.jsx
@@ -0,0 +1,27 @@
+import "./Resume.css";
+import React from 'react'
+
+const AboutMe = () => {
+
+  return (
+    <div id="resume-parent">
+      <h1 className="resume-title" id="projects">Resume</h1>
+
+      <div className="resume-container">
+        <div>
+          <a href="#">
+            <img
+              src="https://ibb.co/ZcZwKrt"
+              alt="resume"
+              border="0"
+              className="thing1"
+            />
+          </a>
+      </div>
+      <hr className="line3" />
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
